b2669224df0501c335ea44eac5ffcd11d89cf9cd
anaskhafegui <anas.mdhat@gmail.com>
2020-02-15 17:38:46 +0200

    Facade Pattern

--- Structural/index.php
+++ Structural/index.php
@@ -9,6 +9,9 @@ use DesignPatternsInPHP\Structural\Decorator\OilChange;
 use DesignPatternsInPHP\Structural\Decorator\TireRotation;
 use DesignPatternsInPHP\Structural\Decorator\BasicInspection;
 
+use DesignPatternsInPHP\Structural\Facade\ComputerFacade;
+use DesignPatternsInPHP\Structural\Facade\Computer;
+
 
 (new Person)->read(new KindleAdapter(new Kindle));
 
@@ -18,4 +21,8 @@ echo '<hr>';
 $service = new OilChange(new TireRotation (new BasicInspection));
 
 echo $service->getDescription();
+echo '<hr>';
 
+$computer = new ComputerFacade(new Computer());
+$computer->turnOn(); // Ouch! Beep beep! Loading.. Ready to be used!
+$computer->turnOff(); // Bup bup buzzz! Haah! Zzzzz
